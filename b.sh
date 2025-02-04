bbae4be896f47fa2f90ed9d65b50c973dd9aa21c
faisal426 <github7251gmail.com>
2025-02-05 00:28:39 +0700

    backup

--- b.sh
+++ b.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Cek apakah ada direktori .git di dalam direktori saat ini
-if [ ! -d ".git" ]; then
-    echo "Error: Direktori ini bukan repositori Git!"
-    exit 1
-fi
-
 # Menambahkan perubahan
 git add .
 
@@ -13,7 +7,7 @@ git add .
 git commit -m "backup"
 
 # Push ke branch master
-git push -u origin master
+git push --force  origin master
 
 # Tampilkan pesan backup selesai dengan warna hijau
 echo -e "\e[32mBackup selesai!\e[0m"
