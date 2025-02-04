0a8e33a6b6be91a3c862a5fc899eed91d8a3987a
faisal426 <ghub725@gmail.com>
2025-02-05 05:48:39 +0700

    backup

--- b.sh
+++ b.sh
@@ -9,5 +9,5 @@ git commit -m "backup"
 # Push ke branch master
 git push origin master
 
-# Tampilkan pesan backup selesai dengan warna hijau
-echo "Backup selesai!"
+# Tampilkan pesan backup selesai dengan warna hijau cerah pada latar belakang hitam
+echo -e "\e[1;92m\e[40mBackup selesai!\e[0m"
